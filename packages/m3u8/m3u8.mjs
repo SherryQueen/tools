655829fe708bfe2d13334a4ab6a91a27993fcee8
56 <mysherrymyqueen@outlook.com>
2024-07-27 19:01:16 +0800

    feat: m3u8

--- packages/m3u8/m3u8.mjs
+++ packages/m3u8/m3u8.mjs
@@ -1,34 +1,44 @@
 import { readFileSync, writeFileSync } from 'node:fs';
+import { URL } from 'node:url';
 import { isAbsolute, join, parse } from 'node:path';
 
 import fetch from 'node-fetch';
 
 const config = JSON.parse(readFileSync('./config.json', 'utf8'));
 
-const url = process.argv[2];
-const baseUrl = config.cdn;
+async function download(link) {
+  console.info(`download url: ${link}`);
+  const content = await fetch(link).then((res) => res.text());
 
-const content = url ? await fetch(url).then((res) => res.text()) : readFileSync(config.output.m3u8, 'utf-8');
-const lines = content.split('\n');
-const urls = [];
-const chunks = [];
-for (const line of lines) {
-  if (line.endsWith('.ts')) {
-    urls.push(line.startsWith('http') || line.startsWith('https') ? line : baseUrl + line);
+  const lines = content.split('\n');
+  const urls = [];
+  const chunks = [];
+  for (const line of lines) {
+    if (line.endsWith('.ts')) {
+      urls.push(line.startsWith('http') || line.startsWith('https') ? line : baseUrl + line);
 
-    const pathObj = parse(line);
-    const filename = pathObj.name + pathObj.ext;
-    chunks.push(filename);
-  } else chunks.push(line);
+      const pathObj = parse(line);
+      const filename = pathObj.name + pathObj.ext;
+      chunks.push(filename);
+    } else if (line.endsWith('.m3u8')) {
+      const u = new URL(link);
+      const url = `${u.protocol}//${u.host}${line}`;
+      console.info(`identify the url: ${url}`);
+      download(url);
+      return;
+    } else chunks.push(line);
+  }
+
+  writeFileSync(
+    isAbsolute(config.output.urls) ? config.output.urls : join(process.cwd(), config.output.urls),
+    urls.join('\n'),
+    'utf8',
+  );
+  writeFileSync(
+    isAbsolute(config.output.m3u8) ? config.output.m3u8 : join(process.cwd(), 'index.m3u8'),
+    chunks.join('\n'),
+    'utf8',
+  );
 }
 
-writeFileSync(
-  isAbsolute(config.output.urls) ? config.output.urls : join(process.cwd(), config.output.urls),
-  urls.join('\n'),
-  'utf8',
-);
-writeFileSync(
-  isAbsolute(config.output.m3u8) ? config.output.m3u8 : join(process.cwd(), 'index.m3u8'),
-  chunks.join('\n'),
-  'utf8',
-);
+download(process.argv[2]);
